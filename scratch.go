89303beb5477174b9ec484a1097273b02d5553b0
Ian Gregson <hello@iangregson.com>
2024-07-29 08:10:59 +0100

    Update

--- scratch.go
+++ scratch.go
@@ -2,4 +2,8 @@ package main
 
 func hello() {
 	// This is just a comment
+
+	myMap := map[string]string{"first key": "first value", "second key": "second value", "third key": "third value"}
+
+	_ = myMap
 }
